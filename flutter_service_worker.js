cee25eb82630242fe086ccd44033ee51d0b6d04a
songhuaixu <songhuaixu@gmail.con>
2020-08-03 19:23:09 +0800

    Update Web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "db40952a6e0e01508d8534d677a03bd3",
 "/": "db40952a6e0e01508d8534d677a03bd3",
-"main.dart.js": "e3d1f2cbc1f166a3f8d468480de4d20e",
+"main.dart.js": "3e5f742da3be17d479febcc020330673",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
