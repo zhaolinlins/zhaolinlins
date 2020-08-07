655eb8ec5a286a7bc52f99a0ef33d85a906b0007
zhaolinlin <android_zl@163.com>
2020-08-07 08:38:00 +0800

    Update Web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "db40952a6e0e01508d8534d677a03bd3",
 "/": "db40952a6e0e01508d8534d677a03bd3",
-"main.dart.js": "dc9c41701ebaa7b3d8b18a8571943264",
+"main.dart.js": "0ec6155c595eb3959d51eca78187206e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
